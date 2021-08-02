4d9a75e7b33af1c98885147b563f91511baa61f9
yooooyo <loipswqe@gmail.com>
2021-08-02 16:32:14 +0800

    Feat: create database

--- db.py
+++ db.py
@@ -1,11 +1,58 @@
 import sqlite3
+import os
+from pathlib import Path
 
 class db:
 
-    def __init__(self,path)
-        pass
+    def __init__(self,path):
+        self.path = Path(path)
+        if not self.path.exists():
+            self.__create_table()
 
     def __create_table(self):
-        pass
+        con = sqlite3.connect(self.path.absolute().as_posix())
+        try:
+            cur = con.cursor()
+            cur.executescript(
+                '''
+                CREATE TABLE uuts(
+                    id INTEGER PRIMARY KEY,
+                    platform VARCHAR(50),
+                    sn VARCHAR(50),
+                    bios VARCHAR(50),
+                    os VARCHAR(50),
+                    operator VARCHAR(50),
+                    imsi VARCHAR(50),
+                    iccid VARCHAR(50),
+                    module text,
+                    tracker_version VARCHAR(50)
+                );
+
+                CREATE TABLE ftdata(
+                    id INTEGER PRIMARY KEY,
+                    uut_id INTEGER,
+                    time DATETIME,
+                    lat text,
+                    lng text,
+                    status VARCHAR(50),
+                    apn VARCHAR(50),
+                    cellid VARCHAR(50),
+                    rsrp_rssi VARCHAR(50),
+                    bandinfo  VARCHAR(50),
+                    error_status text,
+                    FOREIGN KEY (uut_id) REFERENCES uuts(id)
+                );
+                '''
+            )
+            con.close()
+        except:
+            con.close()
+            os.remove(self.path.absolute().as_posix())
+
+def create_db():
+    db('tracker_data.db')
+
+
+    
 
     
